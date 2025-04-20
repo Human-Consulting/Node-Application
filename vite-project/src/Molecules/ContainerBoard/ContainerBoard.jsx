a63dc2e35fdec6b32c9531ce32d4e1e45e188326
Joao-Nocee <joao.noce@sptech.school>
2025-04-20 16:36:54 -0300

    Modificações no login e remoção do cadastro

--- vite-project/src/Molecules/ContainerBoard/ContainerBoard.jsx
+++ vite-project/src/Molecules/ContainerBoard/ContainerBoard.jsx
@@ -2,21 +2,15 @@ import { Alert, Divider, Snackbar, Stack } from "@mui/material";
 import { ButtonMeu, LoginBack, LoginTitulo, InputMinha } from "./ContainerBoard.styles";
 import { useNavigate } from "react-router";
 import { handleSubmitLogin } from "../../Utils/UsePost";
-import { handleSubmitCadastro } from "../../Utils/UseCadastro";
 import React from "react";
 
 const ContainerBoard = () => {
   const navigate = useNavigate();
-  const [loginType, setLoginType] = React.useState(true);
   const [responseMessage, setResponseMessage] = React.useState('');
   const [responseSucess, setResponseSucess] = React.useState('');
   const [loading, setLoading] = React.useState(false);
   const [emailLogin, setEmailLogin] = React.useState("");
   const [senhaLogin, setSenhaLogin] = React.useState("");
-  const [nomeCadastro, setNomeCadastro] = React.useState("");
-  const [emailCadastro, setEmailCadastro] = React.useState("");
-  const [senhaCadastro, setSenhaCadastro] = React.useState("");
-  const [confirmarSenhaCadastro, setConfirmarSenhaCadastro] = React.useState("");
 
   return (
     <LoginBack>
@@ -39,137 +33,54 @@ const ContainerBoard = () => {
         <p>Seu projeto, nossa ajuda e seu resultado</p>
       </Stack>
 
-      {loginType ? (
-        <Stack sx={{ gap: '0.7rem' }}>
-          <p>Entre e tenha total controle de seus projetos</p>
-          <Stack gap={{ gap: '1rem' }}>
-            <InputMinha
-              sx={{
-                input: {
+      <Stack sx={{ gap: '0.7rem' }}>
+        <p>Entre e tenha total controle de seus projetos</p>
+        <Stack gap={{ gap: '1rem' }}>
+          <InputMinha
+            sx={{
+              input: {
+                color: 'white',
+                "&::placeholder": {
+                  opacity: 1,
                   color: 'white',
-                  "&::placeholder": {
-                    opacity: 1,
-                    color: 'white',
-                  },
                 },
-                label: { color: 'white' },
-              }}
-              label="E-mail"
-              value={emailLogin}
-              onChange={(e) => setEmailLogin(e.target.value)}
-            />
-            <InputMinha
-              sx={{
-                input: {
+              },
+              label: { color: 'white' },
+            }}
+            label="E-mail"
+            value={emailLogin}
+            onChange={(e) => setEmailLogin(e.target.value)}
+          />
+          <InputMinha
+            sx={{
+              input: {
+                color: 'white',
+                "&::placeholder": {
+                  opacity: 1,
                   color: 'white',
-                  "&::placeholder": {
-                    opacity: 1,
-                    color: 'white',
-                  },
                 },
-                label: { color: 'white' },
-              }}
-              label="Senha"
-              type="password"
-              value={senhaLogin}
-              onChange={(e) => setSenhaLogin(e.target.value)}
-            />
-            <ButtonMeu onClick={() => handleSubmitLogin(emailLogin, senhaLogin, navigate, setResponseMessage, setLoading)}>
-              {loading ? 'Carregando' : 'Confirmar'}
-            </ButtonMeu>
-          </Stack>
+              },
+              label: { color: 'white' },
+            }}
+            label="Senha"
+            type="password"
+            value={senhaLogin}
+            onChange={(e) => setSenhaLogin(e.target.value)}
+          />
+          <ButtonMeu onClick={() => handleSubmitLogin(emailLogin, senhaLogin, navigate, setResponseMessage, setLoading)}>
+            {loading ? 'Carregando' : 'Confirmar'}
+          </ButtonMeu>
         </Stack>
-      ) : (
-        <Stack sx={{ gap: '0.7rem' }}>
-          <p>Cadastre-se e não perca tempo</p>
-          <Stack gap={{ gap: '1rem' }}>
-            <InputMinha
-              sx={{
-                input: {
-                  color: 'white',
-                  "&::placeholder": {
-                    opacity: 1,
-                    color: 'white',
-                  },
-                },
-                label: { color: 'white' },
-              }}
-              label="Nome"
-              value={nomeCadastro}
-              onChange={(e) => setNomeCadastro(e.target.value)}
-            />
-            <InputMinha
-              sx={{
-                input: {
-                  color: 'white',
-                  "&::placeholder": {
-                    opacity: 1,
-                    color: 'white',
-                  },
-                },
-                label: { color: 'white' },
-              }}
-              label="E-mail"
-              value={emailCadastro}
-              onChange={(e) => setEmailCadastro(e.target.value)}
-            />
-            <InputMinha
-              sx={{
-                input: {
-                  color: 'white',
-                  "&::placeholder": {
-                    opacity: 1,
-                    color: 'white',
-                  },
-                },
-                label: { color: 'white' },
-              }}
-              label="Senha"
-              type="password"
-              value={senhaCadastro}
-              onChange={(e) => setSenhaCadastro(e.target.value)}
-            />
-            <InputMinha
-              sx={{
-                input: {
-                  color: 'white',
-                  "&::placeholder": {
-                    opacity: 1,
-                    color: 'white',
-                  },
-                },
-                label: { color: 'white' },
-              }}
-              label="Confirmar senha"
-              type="password"
-              value={confirmarSenhaCadastro}
-              onChange={(e) => setConfirmarSenhaCadastro(e.target.value)}
-            />
-            <ButtonMeu onClick={() => handleSubmitCadastro(nomeCadastro, emailCadastro, senhaCadastro, confirmarSenhaCadastro, setLoading, setResponseMessage, setResponseSucess)}>
-              {loading ? 'Carregando' : 'Confirmar'}
-            </ButtonMeu>
-          </Stack>
-        </Stack>
-      )}
+      </Stack>
 
-      <Stack sx={{ position: 'absolute', bottom: '3rem', gap: '0.5rem' }}>
-        <Divider sx={{ color: '#fff', background: '#fff', width: '400px' }} />
-
-        {loginType ? (
-          <p style={{ fontSize: '16px' }}>
-            Ainda não tenho uma conta e gostaria de se{' '}
-            <b onClick={() => setLoginType(false)} style={{ color: 'rgba(89,194,255,1)', cursor: 'pointer' }}>
-              Cadastrar
-            </b>
-          </p>
-        ) : (
-          <p style={{ fontSize: '16px' }}>
-            Caso já tenha uma conta, clique{' '}
-            <b onClick={() => setLoginType(true)} style={{ color: 'rgba(89,194,255,1)', cursor: 'pointer' }}>
-              Aqui
-            </b>
-          </p>
-        )}
+      <Stack sx={{ gap: '0.5rem', alignItems: 'center' }}>
+        <Divider sx={{ color: '#fff', background: '#fff', width: '100%' }} />
+        <p style={{ fontSize: '16px' }}>
+          {' '}
+          <b onClick={() => console.log("Esqueci minha senha")} style={{ color: 'rgba(89,194,255,1)', cursor: 'pointer' }}>
+            Esqueci minha senha
+          </b>
+        </p>
       </Stack>
     </LoginBack>
   );
--- vite-project/src/Molecules/ContainerBoard/ContainerBoard.styles.jsx
+++ vite-project/src/Molecules/ContainerBoard/ContainerBoard.styles.jsx
@@ -2,62 +2,65 @@ import { Box, Button, TextField } from '@mui/material';
 import { styled } from '@mui/system';
 
 export const LoginBack = styled(Box)({
-    width: '30vw',
-    height: '95vh',
-    background: "#1d1d1dc8",
-    backdropFilter: 'blur(10px)',
-    display: 'flex',
-    flexDirection: 'column',
-    borderRadius: '5px',
-    position: 'relative',
-    zIndex: '30',
-    padding: '2rem',
-    gap: '3.5rem',
+  width: '30vw',
+  height: '95vh',
+  background: "#1d1d1dc8",
+  backdropFilter: 'blur(10px)',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  borderRadius: '5px',
+  position: 'relative',
+  zIndex: '30',
+  paddingInline: '2rem',
+  paddingBlock: '4rem',
 });
+
 export const LoginTitulo = styled('h2')({
-    fontFamily: 'Oswald, sans-serif',
-    fontWeight: 400,
-    fontSize: '42px',
-    lineHeight: '46px',
-    color: '#fff'
+  fontFamily: 'Oswald, sans-serif',
+  fontWeight: 400,
+  fontSize: '42px',
+  lineHeight: '46px',
+  color: '#fff'
 });
+
 export const Container = styled(Box)({
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    display: 'flex',
-    position: 'absolute',
-    alignItems: 'center',
-    padding: '1rem'
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: 'flex',
+  position: 'absolute',
+  alignItems: 'center',
+  padding: '1rem'
 });
 
 export const InputMinha = styled(TextField)({
-    '& label.Mui-focused': {
-      color: '#ffffff',
+  '& label.Mui-focused': {
+    color: '#ffffff',
+  },
+  "&::placeholder": {
+    color: "gray"
+  },
+  color: "white",
+
+
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: '#E0E3E7',
     },
-        "&::placeholder": {
-          color: "gray"
-        },
-        color: "white",
-     
-  
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: '#E0E3E7',
-      },
-      '&:hover fieldset': {
-        borderColor: '#B2BAC2',
-      },
-      '&.Mui-focused fieldset': {
-        borderColor: '#6F7E8C',
-      },
+    '&:hover fieldset': {
+      borderColor: '#B2BAC2',
     },
-  });
+    '&.Mui-focused fieldset': {
+      borderColor: '#6F7E8C',
+    },
+  },
+});
 
 export const ButtonMeu = styled(Button)({
-    background: '#1c8bcb',
-    height: '56px',
-    color: '#fff'
+  background: '#1c8bcb',
+  height: '56px',
+  color: '#fff'
 
 });
